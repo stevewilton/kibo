6fcba64c139dd508cd0dbb6da7a0c5f7d55c0c46
Steve Wilton <stevewilton@vlan-2695-10-17-136-81.guest.wireless.sydney.edu.au>
2017-11-20 10:25:29 +1100

    Added FM code

---
+++ RunFMAnomalies.py
@@ -0,0 +1,87 @@
+'''
+Run file
+'''
+
+import numpy as np
+import random
+import collections
+import time
+import math
+from FixedPoint import FixedPoint
+from Lstm import LSTM
+from Dense import Dense
+from Settings import *
+from Network import *
+from WorkloadFMAnomalies import *
+
+
+# Hyper-parameters
+FFT_WINDOW_SIZE = 32
+LAYER_SIZES = [16,8,16,32]
+
+print "Constructing data set"
+
+training_set, validation_set, test_set = WorkloadFMAnomalies(FFT_WINDOW_SIZE)
+
+
+print "Initializing Network"
+
+network = Network(FFT_WINDOW_SIZE)
+for layer_size in LAYER_SIZES:
+    network.add_level(0,layer_size)
+
+print "Training Network:"
+
+for i in range(0,len(training_set)):
+   if i % 100 == 0:
+      print "training example %d/%d" % (i, len(training_set) )
+
+   training_example_x = training_set[i][0]
+   training_example_y = training_example_x
+
+   o = network.train(training_example_x, training_example_y, 0.1)
+
+
+print "Trained network:"
+network.print_weights_and_biases()
+
+print "Testing network"
+
+anomaly_correct = 0  # how many times does it correctly predict an anomaly
+anomaly_wrong = 0  # how many times does it incorrectly predict an anomaly
+true_correct = 0 # how many times does it correctly predict a non-anomaly
+true_wrong = 0 # how manhy times does it incorrectly predict a non-anomaly
+
+for i in range(0, len(test_set)):
+
+   if i % 10 == 0:
+      print "test example %d/%d" % (i, len(test_set))
+
+   test_example_x = test_set[i][0]
+   test_example_y = test_set[i][1]
+
+   o = network.forward_pass(test_example_x)
+   diff = np.sum((o-test_example_x)*(o-test_example_x))
+
+   # use an arbitary threshold of 0.5
+   # why? because it works
+
+   if (test_example_y):  # if this was supposed to be an anomaly
+      if (diff > 0.5):
+          anomaly_correct += 1
+      else:
+          anomaly_wrong += 1
+   else:   # it was not supposed to be an anomaly
+      if (diff > 0.5):
+          true_wrong += 1
+      else:
+          true_correct += 1
+
+
+print "Of the %d anomalies:" % (anomaly_correct + anomaly_wrong)
+print "    correctly predicted =   %d" % anomaly_correct
+print "    incorrectly predicted = %d" % anomaly_wrong
+print "Of the %d non-anomalies:" % (true_correct + true_wrong)
+print "    correctly predicted   = %d" % true_correct
+print "    incorrectly predicted = %d" % true_wrong
+
---
+++ WorkloadFMAnomalies.py
@@ -0,0 +1,91 @@
+'''
+Workload which is a string of characters from a file
+'''
+
+import numpy as np
+import scipy
+import random
+import collections
+import time
+import math
+from FixedPoint import FixedPoint
+from Settings import *
+
+# Hyper-parameters
+DATASET_FILE = "fm_data.bin"
+NUM_TRAINING_EXAMPLES = 10000
+TRAIN_SET_RATIO = 0.8
+TEST_SET_RATIO = 0.1
+GAUSSIAN_NOISE_SD = 0.15
+
+def load_real_data():
+    x = scipy.fromfile(open(DATASET_FILE), dtype=scipy.complex64)
+    i_s = np.real(x[:NUM_TRAINING_EXAMPLES])
+    q_s = np.imag(x[:NUM_TRAINING_EXAMPLES])
+    return i_s,q_s
+
+def make_data(data, window_size):
+    X = []
+    win_size = int(window_size)
+    for i in range(len(data)-win_size):
+        win = data[i:i+win_size]
+        X.append(win)
+    return np.array(X)
+
+def make_fft_windows(i,q,window_size):
+    sig = i + q*1j;
+    sig_window = make_data(sig, window_size)
+    data = []
+    for sig_w in sig_window:
+        fft_res = np.fft.fft(sig_w)
+        fft_r = np.real(fft_res)
+        fft_i = np.imag(fft_res)
+        data.append(np.concatenate((fft_r,fft_i)).tolist())
+    return data
+
+def convert_to_list(c):
+    l = [[x] for x in c]
+    return l
+
+def WorkloadFMAnomalies(window_size):
+    i_s,q_s = load_real_data();
+    data = make_fft_windows(i_s,q_s,int(window_size/2))
+
+    training_set_x = []
+    training_set_y = []
+    test_set_x = []
+    test_set_y = []
+    validation_set_x = []
+    validation_set_y = []
+
+    train_size = int(len(data)*TRAIN_SET_RATIO)
+    test_size = int(len(data)*TEST_SET_RATIO)
+
+    count = 0
+    for i in range(0,train_size):
+        training_set_x.append(convert_to_list(data[i]))
+        training_set_y.append(0)
+    for i in range(train_size,train_size+test_size):
+        if (random.randint(1,8) == 1):
+            y = 1
+            noise = data[i] + np.random.normal(0,GAUSSIAN_NOISE_SD,32) # add some gaussian noise
+        else:
+            noise = data[i]
+            y = 0
+        test_set_x.append(convert_to_list(noise))
+        test_set_y.append(y)
+    for i in range(train_size+test_size,len(data)):
+        if (random.randint(1,8) == 1):
+            y = 1
+            noise = data[i] + np.random.normal(0,GAUSSIAN_NOISE_SD,32) # add some gaussian noise
+        else:
+            noise = data[i]
+            y = 0
+        validation_set_x.append(convert_to_list(noise))
+        validation_set_y.append(y)
+
+    training_set = zip(training_set_x, training_set_y)
+    validation_set = zip(validation_set_x, validation_set_y)
+    test_set = zip(test_set_x, test_set_y)
+
+    return((training_set, validation_set, test_set))
